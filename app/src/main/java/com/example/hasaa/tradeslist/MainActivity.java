04b207d582cc6802f76e336b5e219fb47ca23376
Hasaan <Hasaan@users.noreply.github.com>
2017-11-13 14:57:33 -0800

    added main activity now goes to view profile

--- app/src/main/java/com/example/hasaa/tradeslist/MainActivity.java
+++ app/src/main/java/com/example/hasaa/tradeslist/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.hasaa.tradeslist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void view_profile_button(View v) {
+
+        Button b = (Button)v;
+        //b.setText("make a string");
+        startActivity(new Intent(MainActivity.this,ViewProfileActivity.class));
     }
 }
