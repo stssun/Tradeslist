808d26746d46e0ce934bf900e7b29c6cd779f5ab
stssun <stssun@ucsc.edu>
2017-11-13 15:22:19 -0800

    View Profile Activity Buttons and Fields
    Also created two new activities for displaying the editing screen for the profile and the listings.

--- app/src/main/java/com/example/hasaa/tradeslist/ViewProfileActivity.java
+++ app/src/main/java/com/example/hasaa/tradeslist/ViewProfileActivity.java
@@ -1,7 +1,10 @@
 package com.example.hasaa.tradeslist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ViewProfileActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class ViewProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_profile);
+    }
+
+    //called when the user pushes the edit profile button
+    public void editProfile(View view){
+        Intent intent = new Intent(this, EditProfileActivity.class);
+        startActivity(intent);
+    }
+
+    //called when the user pushes the my listings button
+    public void viewMyListings(View view){
+        Intent intent = new Intent(this, MyListingsActivity.class);
+        startActivity(intent);
     }
 }
