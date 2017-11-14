b42e30d0c28ec4c12ca4df6e2f539710c73e5e90
Hasaan <Hasaan@users.noreply.github.com>
2017-11-13 17:00:51 -0800

    added few buttons/text, neatened up
    home screen has better looking buttons
    
    added edit texts for post page, and gave it button to finish() and return. need to have function to post to firebase before finish()

--- app/src/main/java/com/example/hasaa/tradeslist/PostActivity.java
+++ app/src/main/java/com/example/hasaa/tradeslist/PostActivity.java
@@ -1,7 +1,10 @@
 package com.example.hasaa.tradeslist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PostActivity extends AppCompatActivity {
 
@@ -10,4 +13,14 @@ public class PostActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
     }
+
+
+    public void post_button(View v) {
+
+        Button b = (Button)v;
+        //b.setText("make a string");
+//        startActivity(new Intent(PostActivity.this,HomeActivity.class));
+        finish();
+    }
+
 }
