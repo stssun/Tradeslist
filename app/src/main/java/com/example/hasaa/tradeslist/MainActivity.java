978439230450787f2459b252f83e8d6ec538d6b0
stssun <stssun@ucsc.edu>
2017-11-27 19:02:25 -0800

    Finished setting up the Sign In
    Havent yet tested to see if it truly works with data pulls

--- app/src/main/java/com/example/hasaa/tradeslist/MainActivity.java
+++ app/src/main/java/com/example/hasaa/tradeslist/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.hasaa.tradeslist;
 /**
  * Most of the authentication code is adapted or taken from the official firebase android studio
  * user authentication tutorial on github:
- * https://developers.google.com/identity/sign-in/android/sign-in?authuser=0
+ * https://developers.google.com/identity/sign-in/android/sign-in
  */
 
 import android.content.Intent;
@@ -22,15 +22,19 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private SignInButton mGoogleButton;
     private GoogleSignInClient mGoogleSignInClient;
+
+    //constant for sign in request value
+    private static final int RC_SIGN_IN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
 
         //set button
         mGoogleButton = (SignInButton) findViewById(R.id.google_button);
+        //set listener
+        mGoogleButton.setOnClickListener(this);
 
         //configure sign-in to request user id, email address and basic profile
         //taken from tutorial
@@ -63,6 +69,59 @@ public class MainActivity extends AppCompatActivity {
         if(account != null){
             Intent intent = new Intent(this, HomeActivity.class);
             startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onClick(View v){
+        //do log in and stuff
+        //following the tutorial
+        switch(v.getId()){
+            case R.id.google_button:
+                signIn();
+                break;
+        }
+    }
+
+    //taken from tutorial
+    //starts the sign-in intent which is the activity with the different email options and
+    //asks for permissions
+    private void signIn(){
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    //taken from tutorial
+    //once the sign in activity is called
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    //taken from tutorial
+    //handles the sign in result
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, go to home activity
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("MainActivity", "signInResult:failed code=" + e.getStatusCode());
+
+            //perform some action since sign-in failed
         }
     }
 }
